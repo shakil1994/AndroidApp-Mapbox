98b7fe46ca114cb4e3a36cfc56f453d85100e2f8
android_shakil <shakhawathossen53@gmail.com>
2020-12-07 16:17:26 +0600

    Kotlin Add a marker on click

--- KotlinMapboxExample/app/src/main/java/com/example/shakil/kotlinmapboxexample/MainActivity.kt
+++ KotlinMapboxExample/app/src/main/java/com/example/shakil/kotlinmapboxexample/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.shakil.kotlinmapboxexample
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Toast
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponent
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
     private var mapboxMap: MapboxMap? = null
     private var permissionsManager: PermissionsManager? = null
@@ -33,7 +40,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
         mapboxMap.setStyle(getString(R.string.navigation_guidance_day)) {
             style: Style? ->
             enableLocationComponent(style)
+            addDestinationIconSymbolLayer(style)
+            mapboxMap.addOnMapClickListener(this)
         }
+    }
+
+    private fun addDestinationIconSymbolLayer(loadedMapStyle: Style?) {
+        loadedMapStyle!!.addImage("destination-icon-id", BitmapFactory.decodeResource(this.resources, R.drawable.mapbox_marker_icon_default))
+
+        val geoJsonSource = GeoJsonSource("destination-source-id")
+        loadedMapStyle.addSource(geoJsonSource)
+        val destinationSymbolLayer = SymbolLayer("destination-symbol-layer-id", "destination-source-id")
+        destinationSymbolLayer.withProperties(PropertyFactory.iconImage("destination-icon-id"),
+        PropertyFactory.iconAllowOverlap(true),
+        PropertyFactory.iconIgnorePlacement(true))
+
+        loadedMapStyle.addLayer(destinationSymbolLayer)
+    }
+
+    override fun onMapClick(point: LatLng): Boolean {
+        val destinationPoint = Point.fromLngLat(point.longitude, point.latitude)
+        val originPoint = Point.fromLngLat(locationComponent!!.lastKnownLocation!!.longitude, locationComponent!!.lastKnownLocation!!.latitude)
+
+        val source = mapboxMap!!.style!!.getSourceAs<GeoJsonSource>("destination-source-id")
+        source?.setGeoJson(Feature.fromGeometry(destinationPoint))
+
+        return true
     }
 
     private fun enableLocationComponent(loadedMapStyle: Style?) {
@@ -107,6 +139,4 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
         super.onLowMemory()
         mapView.onLowMemory()
     }
-
-
 }
