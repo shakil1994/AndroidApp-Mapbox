2321700cdd9081da71802be077dbdf8d4cbdb102
android_shakil <shakhawathossen53@gmail.com>
2020-12-07 16:54:05 +0600

    Kotlin Calculate and draw route

--- KotlinMapboxExample/app/src/main/java/com/example/shakil/kotlinmapboxexample/MainActivity.kt
+++ KotlinMapboxExample/app/src/main/java/com/example/shakil/kotlinmapboxexample/MainActivity.kt
@@ -4,9 +4,13 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Toast
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.directions.v5.models.DirectionsWaypoint
 import com.mapbox.geojson.Feature
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
@@ -19,13 +23,21 @@ import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
     private var mapboxMap: MapboxMap? = null
     private var permissionsManager: PermissionsManager? = null
     private var locationComponent: LocationComponent? = null
+    private var currentRoute: DirectionsRoute? = null
+    private var navigationMapRoute: NavigationMapRoute? = null
+    private val TAG = "DirectionsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,7 +77,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
         val source = mapboxMap!!.style!!.getSourceAs<GeoJsonSource>("destination-source-id")
         source?.setGeoJson(Feature.fromGeometry(destinationPoint))
 
+        getRoute(originPoint, destinationPoint)
         return true
+    }
+
+    private fun getRoute(origin: Point, destination: Point) {
+        NavigationRoute.builder(this).accessToken(Mapbox.getAccessToken()!!).origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(object : Callback<DirectionsResponse> {
+                    override fun onResponse(call: Call<DirectionsResponse>, response: Response<DirectionsResponse>) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.body())
+                        if (response.body() == null) {
+                            Log.d(TAG, "No routes found, make sure you set the right user and access token")
+                            return
+                        }
+                        else if (response.body()!!.routes().size < 1){
+                            Log.e(TAG, "No routes found")
+                            return
+                        }
+                        currentRoute = response.body()!!.routes()[0]
+
+                        //Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute!!.removeRoute()
+                        }
+                        else {
+                            navigationMapRoute = NavigationMapRoute(null, mapView, mapboxMap!!, R.style.NavigationMapRoute)
+                        }
+                        navigationMapRoute!!.addRoute(currentRoute)
+                    }
+
+                    override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                        Log.e(TAG, "Error: " + t.message)
+                    }
+
+                })
     }
 
     private fun enableLocationComponent(loadedMapStyle: Style?) {
