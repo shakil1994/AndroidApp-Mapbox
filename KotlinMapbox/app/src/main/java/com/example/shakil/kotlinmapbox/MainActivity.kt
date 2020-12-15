2c4efee9c9072eb4fa1a5a310e0eedc372e1b806
android_shakil <shakhawathossen53@gmail.com>
2020-12-16 01:28:22 +0600

    Add Serach button

--- KotlinMapbox/app/src/main/java/com/example/shakil/kotlinmapbox/MainActivity.kt
+++ KotlinMapbox/app/src/main/java/com/example/shakil/kotlinmapbox/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.shakil.kotlinmapbox
 
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.api.directions.v5.models.DirectionsResponse
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponent
 import com.mapbox.mapboxsdk.location.modes.CameraMode
@@ -22,9 +27,12 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.MapboxMap.OnMapClickListener
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.mapboxsdk.utils.BitmapUtils
 import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
 import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
 import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
@@ -37,12 +45,16 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener,
     OnMapClickListener {
 
+    private val REQUEST_CODE_AUTOCOMPLETE = 7171
     private var mapboxMap: MapboxMap? = null
     private var permissionsManager: PermissionsManager? = null
     private var locationComponent: LocationComponent? = null
     private var currentRoute: DirectionsRoute? = null
     private val TAG = "DirectionsActivity"
     private var navigationMapRoute: NavigationMapRoute? = null
+    /********* New Part Search *********/
+    private val geoJsonSourceLayerId = "GeoJsonSourceLayerId"
+    private val symbolIconId = "SymbolIconId"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,16 +64,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
         mapView.getMapAsync(this)
 
         //btnStart = findViewById(R.id.btnStart)
-        btnStart!!.setOnClickListener { v: View? ->
-            val simulateRoute = true
-            val options = NavigationLauncherOptions.builder()
-                .directionsRoute(currentRoute)
-                .shouldSimulateRoute(simulateRoute)
-                .build()
-
-            // Call this method with Context from within an Activity
-            NavigationLauncher.startNavigation(this, options)
-        }
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
@@ -72,6 +74,108 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListene
             enableLocationComponent(style)
             addDestinationIconSymbolLayer(style)
             mapboxMap.addOnMapClickListener(this)
+
+            btnStart!!.setOnClickListener { v: View? ->
+                val simulateRoute = true
+                val options = NavigationLauncherOptions.builder()
+                    .directionsRoute(currentRoute)
+                    .shouldSimulateRoute(simulateRoute)
+                    .build()
+
+                // Call this method with Context from within an Activity
+                NavigationLauncher.startNavigation(this, options)
+            }
+
+            /************* New Part Search  */
+            initSearchFab()
+            //addUserLocations();
+
+            setUpSource(style!!)
+
+            setupLayer(style!!)
+
+            val drawable =
+                ResourcesCompat.getDrawable(resources, R.drawable.ic_location_on_red_24dp, null)
+            val bitmap = BitmapUtils.getBitmapFromDrawable(drawable)
+            style!!.addImage(symbolIconId, bitmap!!)
+        }
+    }
+
+    private fun setupLayer(loadedMapStyle: Style) {
+        loadedMapStyle.addLayer(
+            SymbolLayer("SYMBOL_LAYER_ID", geoJsonSourceLayerId).withProperties(
+                PropertyFactory.iconImage(symbolIconId),
+                PropertyFactory.iconOffset(arrayOf(0f, -8f))
+            )
+        )
+    }
+
+    private fun setUpSource(loadedMapStyle: Style) {
+        loadedMapStyle.addSource(GeoJsonSource(geoJsonSourceLayerId))
+    }
+
+    private fun initSearchFab() {
+        fab_location_search.setOnClickListener { v: View? ->
+            val intent = PlaceAutocomplete.IntentBuilder()
+                .accessToken(
+                    (if (Mapbox.getAccessToken() != null) Mapbox.getAccessToken() else getString(
+                        R.string.access_token
+                    ))!!
+                )
+                .placeOptions(
+                    PlaceOptions.builder()
+                        .backgroundColor(Color.parseColor("#EEEEEE"))
+                        .limit(10) //.addInjectedFeature(home)
+                        //.addInjectedFeature(work)
+                        .build(PlaceOptions.MODE_CARDS)
+                )
+                .build(this@MainActivity)
+            startActivityForResult(
+                intent, REQUEST_CODE_AUTOCOMPLETE
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+
+// Retrieve selected location's CarmenFeature
+            val selectedCarmenFeature = PlaceAutocomplete.getPlace(data)
+            //CarmenFeature carmenFeature = PlacePicker.getPlace(data);
+
+// Create a new FeatureCollection and add a new Feature to it using selectedCarmenFeature above.
+// Then retrieve and update the source designated for showing a selected location's symbol layer icon
+            if (mapboxMap != null) {
+                val style = mapboxMap!!.style
+                if (style != null) {
+                    val source = style.getSourceAs<GeoJsonSource>(geoJsonSourceLayerId)
+                    source?.setGeoJson(
+                        FeatureCollection.fromFeatures(
+                            arrayOf(
+                                Feature.fromJson(
+                                    selectedCarmenFeature.toJson()
+                                )
+                            )
+                        )
+                    )
+
+// Move map camera to the selected location
+                    mapboxMap!!.animateCamera(
+                        CameraUpdateFactory.newCameraPosition(
+                            CameraPosition.Builder()
+                                .target(
+                                    LatLng(
+                                        (selectedCarmenFeature.geometry() as Point?)!!.latitude(),
+                                        (selectedCarmenFeature.geometry() as Point?)!!.longitude()
+                                    )
+                                )
+                                .zoom(14.0)
+                                .build()
+                        ), 4000
+                    )
+                }
+            }
         }
     }
 
